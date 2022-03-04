3815e92314cf7cf80eefa905af54225eb5f0fed9
Zhou Huan <zhouhuan88888@163.com>
2022-03-04 11:15:06 +0800

    feature:<app> 示例Demo Commit

--- simple-app/src/main/java/com/serial/port/kit/MyApp.kt
+++ simple-app/src/main/java/com/serial/port/kit/MyApp.kt
@@ -5,6 +5,10 @@ import android.util.Log
 import com.serial.port.kit.core.SerialPortFinder
 import com.serial.port.manage.SerialPortKit
 import com.serial.port.manage.SerialPortManager
+import com.serial.port.manage.data.WrapReceiverData
+import com.serial.port.manage.data.WrapSendData
+import com.serial.port.manage.listener.OnAddressCheckCall
+import com.serial.port.manage.listener.OnDataCheckCall
 
 /**
  *
@@ -37,12 +41,42 @@ class MyApp : Application() {
         }
 
         portManager = SerialPortKit.newBuilder(this)
+            // 设备地址
             .path("/dev/ttyS0")
+            // 波特率
             .baudRate(115200)
+            // Byte数组最大接收内存
             .maxSize(1024)
+            // 发送失败重试次数
             .retryCount(2)
+            // 发送一次指令，最多接收几次设备发送的数据
+            .receiveMaxCount(1)
+            // 是否按照 maxSize 内存进行接收
+            .isReceiveMaxSize(false)
+            // 是否显示吐司
             .isShowToast(true)
+            // 是否Debug模式，Debug模式会输出Log
             .debug(BuildConfig.DEBUG)
+            // 是否自定义校验下位机发送的数据正确性，把校验好的Byte数组装入WrapReceiverData
+            .isCustom(true, object : OnDataCheckCall {
+                override fun customCheck(
+                    buffer: ByteArray,
+                    size: Int,
+                    onDataPickCall: (WrapReceiverData) -> Unit
+                ): Boolean {
+                    onDataPickCall.invoke(WrapReceiverData(buffer, size))
+                    return true
+                }
+            })
+            // 校验发送指令与接收指令的地址位，相同则为一次正常的通讯
+            .addressCheckCall(object : OnAddressCheckCall {
+                override fun checkAddress(
+                    wrapSendData: WrapSendData,
+                    wrapReceiverData: WrapReceiverData
+                ): Boolean {
+                    return wrapSendData.sendData[1] == wrapReceiverData.data[1]
+                }
+            })
             .build()
             .get()
     }
